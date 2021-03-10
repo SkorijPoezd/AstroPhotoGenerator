6c0a0f573e85cc1538a82e91efa06a6bbec591d6
denis <den.skor94@yandex.ru>
2021-03-10 12:09:00 +0300

    Replace BatchNorm2d to InstanceNorm2d in Generator

--- dcgan/nn_models.py
+++ dcgan/nn_models.py
@@ -1,28 +1,28 @@
 import torch.nn as nn
 
-
+nn.InstanceNorm2d()
 class Generator(nn.Module):
     def __init__(self, z_dim, ngf, n_ch):
         super(Generator, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
             nn.ConvTranspose2d(z_dim, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8, affine=False),
+            nn.InstanceNorm2d(ngf * 8, affine=False),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.Conv2d(ngf * 4, ngf * 4, 3, 1, 1),
-            nn.BatchNorm2d(ngf * 4, affine=False),
+            nn.InstanceNorm2d(ngf * 4, affine=False),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.Conv2d(ngf * 2, ngf * 2, 5, 1, 2),
-            nn.BatchNorm2d(ngf * 2, affine=False),
+            nn.InstanceNorm2d(ngf * 2, affine=False),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.Conv2d(ngf, ngf, 7, 1, 3),
-            nn.BatchNorm2d(ngf, affine=False),
+            nn.InstanceNorm2d(ngf, affine=False),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d(ngf, n_ch, 4, 2, 1, bias=False),
